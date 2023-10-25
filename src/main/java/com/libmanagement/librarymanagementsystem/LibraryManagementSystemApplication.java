1ddcf15ffc89116fa1a9e72ad61bbcb57f37755a
NavrozashviliDamiane <104000748+NavrozashviliDamiane@users.noreply.github.com>
2023-10-25 12:45:28 +0500

    Update LibraryManagementSystemApplication.java
    chnged cors syntax

--- src/main/java/com/libmanagement/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/libmanagement/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -21,7 +21,7 @@ public class LibraryManagementSystemApplication {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("*")
+                    .allowedOriginPatterns("*")
                     .allowedMethods("*")
                     .allowedHeaders("*")
                     .allowCredentials(true);

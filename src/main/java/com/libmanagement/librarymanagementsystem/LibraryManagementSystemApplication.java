c47dbe138924e75b14ed72c74123d84d7a2b2869
NavrozashviliDamiane <104000748+NavrozashviliDamiane@users.noreply.github.com>
2023-10-25 10:41:46 +0500

    Update LibraryManagementSystemApplication.java
    imported cors config

--- src/main/java/com/libmanagement/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/libmanagement/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -1,9 +1,12 @@
 package com.libmanagement.librarymanagementsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
 
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class LibraryManagementSystemApplication {
 
     public static void main(String[] args) {
